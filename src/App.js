b0c8afa83cce0993fc1d365567eb01fee9725f6d
Maksim1108 <maksim.lyanko@bk.ru>
2020-11-16 14:33:16 +0600

    create section "subscribe" and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { Route, Router } from 'react-router-dom';
 import Header from './components/header';
 import Hero from './components/hero';
+import Products from './components/products';
+import video from './components/video';
+import Subscribe from './components/Subscribe';
+import footer from './components/footer';
 import './base.scss';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      {/* <Header />
       <Hero />
+      <Products /> */}
+      <Route exact path='/' component={Hero} />
+      <Route exact path='/products' component={Products} />
+      <Route exact path='/video' component={video} />
+      <Route exact path='/subscribe' component={Subscribe} />
+      <Route exact path='/footer' component={footer} />
     </div>
   );
 };
---
+++ src/components/video/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+const video = () => {
+    return (
+        <section className='video'>
+            <button className='video__btn'></button> 
+        </section>
+    )
+}
+
+export default video
